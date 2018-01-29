55a494980c8e63ebe09c2ff732710c5ba51444ab
huihao.ou <huihao.ou@inveno.com>
2018-01-29 15:09:45 +0800

    修改点：修改错误
    Signed-off-by: huihao.ou <huihao.ou@inveno.com>

--- pythonSpark/spark-mllib/分类/LR/logisticwithSGD.py
+++ pythonSpark/spark-mllib/分类/LR/logisticwithSGD.py
@@ -1,21 +1,27 @@
 import numpy as np
 
 from pyspark.mllib.classification import LogisticRegressionWithSGD
-from pyspark import SparkContext,SparkConf
-from pyspark.mllib.linalg import Vectors, SparseVector
+from pyspark import SparkContext, SparkConf
+from pyspark.mllib.linalg import Vectors
 from pyspark.mllib.regression import LabeledPoint
-
 
 conf = SparkConf().setMaster("local").setAppName("Test")
 
 sc = SparkContext(conf=conf)
 sparse_data = [
- LabeledPoint(0.0, SparseVector(2, {0: 0.0})),
- LabeledPoint(1.0, SparseVector(2, {1: 1.0})),
- LabeledPoint(0.0, SparseVector(2, {0: 1.0})),
- LabeledPoint(1.0, SparseVector(2, {1: 2.0}))
- ]
+    LabeledPoint(0.0, Vectors.dense([1.0, 0.0])),
+    LabeledPoint(1.0, Vectors.dense([0.0, 1.0])),
+    LabeledPoint(0.0, Vectors.dense([10.0, 9.0])),
+    LabeledPoint(1.0, Vectors.dense([9.0, 10.0]))
+]
+sparse_data = [
+    LabeledPoint(0.0, Vectors.dense([1.0, 0.0])),
+    LabeledPoint(1.0, Vectors.dense([0.0, 1.0])),
+    LabeledPoint(0.0, Vectors.dense([10.0, 9.0])),
+    LabeledPoint(1.0, Vectors.dense([9.0, 10.0]))
+]
 rdd = sc.parallelize(sparse_data)
-model = LogisticRegressionWithSGD.train(rdd,iterations=10)
+model = LogisticRegressionWithSGD.train(rdd, iterations=10)
+rdd = rdd.map(lambda x:x.features)
 model.predict(rdd).saveAsTextFile("result/hdfs")
 sc.stop()
